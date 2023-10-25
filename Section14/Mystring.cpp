91b41f804f1484e4be6c62bcfe919c2b2f4daae1
Urban Usling <private@email.com>
2023-10-25 07:19:04 +0200

    Sec14 mystring

--- Section14/Mystring.cpp
+++ Section14/Mystring.cpp
@@ -20,45 +20,48 @@ Mystring::Mystring(const char *s)
         str = new char[std::strlen(s)+1];
         std::strcpy(str, s);
     }
-    std::cout << "const char constructor called for" << str << std::endl;
+    std::cout << "const char constructor called for " << str << std::endl;
 }
 
 Mystring::Mystring(const Mystring &source)
     : str{nullptr} {
         str = new char[std::strlen(source.str) + 1];
         std::strcpy(str, source.str);
-        std::cout << "deep copy constructor called for" << str << std::endl;
+        std::cout << "deep copy constructor called for " << str << std::endl;
     }
 
 // Deconstructor
 Mystring::~Mystring(){
+    std::cout << "Decunstructor called for " << str <<  std::endl;
     delete[] str;
-    std::cout << "Decunstructor called" << std::endl;
+    
 }
 
 Mystring::Mystring(Mystring &&src)
     :str(src.str){
         src.str = nullptr;
-        std::cout << "Move constructor called" << std::endl;
+        std::cout << "Move constructor called for " << str << std::endl;
     }
 
 Mystring &Mystring::operator=(const Mystring &rhs) {// copy assignment
-    std::cout << "Copy assignment called" << std::endl;
+    std::cout << "Copy assignment called for " << str << std::endl;
     if (this == &rhs)
         return *this;
     delete [] str;
     str = new char[strlen(rhs.str) +1];
-    std:strcpy(str, rhs.str);
+    std::strcpy(str, rhs.str);
     return *this;
 }
 
 Mystring &Mystring::operator=(Mystring &&rhs) { // Move assignment
+    if (this == &rhs) {
+        return *this;
+    }
     delete [] this->str;
-    this->str = new char[::strlen(rhs.str) +1];
     this->str = rhs.str;
-    // return this->str;
-
-
+    rhs.str = nullptr;
+    std::cout << "Move assignment called for " << str << std::endl;
+    return *this;
 }
 
 
--- Section14/main.cpp
+++ Section14/main.cpp
@@ -1,13 +1,16 @@
 #include "Mystring.hpp"
 
 int main(){
-    Mystring tom;
-    Mystring fisk{"fisk"};
-    Mystring fisk2{fisk};
+    // Mystring tom;
+    // Mystring fisk{"fisk"};
+    // Mystring fisk2{fisk};
 
-    tom.display();
-    fisk.display();
-    fisk2.display();
+    Mystring a{"hej"};
+    a = Mystring{"Nej"};
+    a = "Bajs";
+    // tom.display();
+    // fisk.display();
+    // fisk2.display();
     
     return 0;
 }
