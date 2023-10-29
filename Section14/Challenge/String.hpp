27715322399a312a8c72aea166809a35ffb06926
Urban Usling <private@email.com>
2023-10-29 21:25:35 +0100

    sec 14 challenge complete

--- Section14/Challenge/String.hpp
+++ Section14/Challenge/String.hpp
@@ -6,6 +6,15 @@
 class String{
     friend std::ostream &operator<<(std::ostream&, const String&);
     friend std::istream &operator>>(std::istream&, String&);
+    friend String operator-(const String &obj);
+    friend String operator + (const String &lhs, const String &rhs);
+    friend bool operator == (const String &lhs, const String &rhs);
+    friend bool operator != (const String &lhs, const String &rhs);
+    friend bool operator < (const String &lhs, const String &rhs);
+    friend bool operator > (const String &lhs, const String &rhs);
+    friend void operator += (String &lhs, const String &rhs);
+    friend String operator * (const String &lhs, size_t);
+    friend void operator *= (String &lhs, size_t);
 
 private:
     char *str;
@@ -17,10 +26,11 @@ public:
     // Constructor
     String();
     String(const char *a);
+    String(const String&);
     // Copy/move
-    String(const String &src);
-    String(String &&src);
+    String &operator=(String &&src);
     // Destructor
+    ~String();
 
     // Operators
     String &operator=(const String&);
--- Section14/Challenge/main.cpp
+++ Section14/Challenge/main.cpp
@@ -4,7 +4,21 @@
 
 int main() {
 
-    String test{"fisk"};
+    String test{"NEJj"};
+    String test2{"BAJS"};
+
+    // test2 = test;
+
+    // String test3{test2};
+
+    // test2 = -test;
+
+    if (test < test2)
+        std::cout << "< fungerar" << std::endl;
+    
+    // std::cin >> test;
+
+    test *= 3;
 
     std::cout << test << std::endl;
 
