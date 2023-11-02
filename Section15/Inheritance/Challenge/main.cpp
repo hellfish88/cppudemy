1b3f7c102022043b58d3e7ed09791eca8d5fd014
Urban Usling <private@email.com>
2023-11-02 07:32:27 +0100

    gitignore

--- Section15/Inheritance/Challenge/main.cpp
+++ Section15/Inheritance/Challenge/main.cpp
@@ -8,7 +8,7 @@ int main(){
 
     Savings_acc test2{"hejsan"};
     Checkings_acc test3;
-    Trust_acc test4;
+    Trust_acc test4{"Förtroendekonto"};
     test4.set_interest(5);
     test4 += 49999;
     test4 += 100001;
@@ -18,16 +18,18 @@ int main(){
     test4 -= 100;
     test4 -= 100;
 
+    test2.deposit(100);
+    test2.withdraw(99);
 
+    test2 += 15;
+    test2.set_interest(15);
+    test2 += 2200;
+    test2 -= 55;
+    test2 -= 200;
+
+        std::cout << test1 << std::endl;
+    std::cout << test2 << std::endl;
+    std::cout << test3 << std::endl;
     std::cout << test4 << std::endl;
-
-    // test2.deposit(100);
-    // test2.withdraw(99);
-
-    // test2 += 15;
-    // test2.set_interest(15);
-    // test2 += 2200;
-    // test2 -= 55;
-    // test2 -= 200;
     return 0;
 }
