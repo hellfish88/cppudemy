46aa3511f472bbc67f25774143457c6e3cb3995d
Urban Usling <private@email.com>
2023-11-06 17:42:32 +0100

    Vet ej

--- Section15/Inheritance/Challenge/main.cpp
+++ Section15/Inheritance/Challenge/main.cpp
@@ -3,33 +3,39 @@
 
 int main(){
 
-    Account test1;
-    test1.deposit(100);
+    // Account test1;
+    // test1.deposit(100);
 
-    Savings_acc test2{"hejsan"};
-    Checkings_acc test3;
-    Trust_acc test4{"Förtroendekonto"};
-    test4.set_interest(5);
-    test4 += 49999;
-    test4 += 100001;
-    test4 += 99999;
-    test4 -= 50;
-    test4 -= 100;
-    test4 -= 100;
-    test4 -= 100;
+    // Savings_acc test2{"hejsan"};
+    // Checkings_acc test3;
+    // Trust_acc test4{"Förtroendekonto"};
+    // test4.set_interest(5);
+    // test4 += 49999;
+    // test4 += 100001;
+    // test4 += 99999;
+    // test4 -= 50;
+    // test4 -= 100;
+    // test4 -= 100;
+    // test4 -= 100;
 
-    test2.deposit(100);
-    test2.withdraw(99);
+    // test2.deposit(100);
+    // test2.withdraw(99);
 
-    test2 += 15;
-    test2.set_interest(15);
-    test2 += 2200;
-    test2 -= 55;
-    test2 -= 200;
+    // test2 += 15;
+    // test2.set_interest(15);
+    // test2 += 2200;
+    // test2 -= 55;
+    // test2 -= 200;
 
-        std::cout << test1 << std::endl;
-    std::cout << test2 << std::endl;
-    std::cout << test3 << std::endl;
-    std::cout << test4 << std::endl;
+    //     std::cout << test1 << std::endl;
+    // std::cout << test2 << std::endl;
+    // std::cout << test3 << std::endl;
+    // std::cout << test4 << std::endl;
+
+    Savings_acc *p1 = new Savings_acc{"Test"};
+
+    p1->deposit(100);
+    p1->withdraw(50);
+
     return 0;
 }
