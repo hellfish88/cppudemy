c655644f78a5366bb1b3c25f6ed20048f6465e80
Urban Usling <private@email.com>
2023-11-20 19:55:55 +0100

    Challenge complete

--- Section20/Challenge_2/musicplayer.cpp
+++ Section20/Challenge_2/musicplayer.cpp
@@ -1,9 +1,10 @@
 #include "Song.hpp"
 #include <list>
-
+#include <limits>
+#include <ios>
 
 void display_playlist (std::list<Song> &songs){
-    for (auto song : songs){
+    for (const auto &song : songs){
         std::cout << song;
     }
 }
@@ -26,11 +27,11 @@ Q - Quit )"
 
 void play_song (std::_List_iterator<Song> &it, std::list<Song> &songs, int num=0) {
     if (num > 0){
-        if (it != songs.end()){
+        if (it != std::prev(songs.end())){
             it++;
         } else {
             it = songs.begin();
-            it++;
+            // it++;
         }
     } else if (num < 0) {
         if (it != songs.begin()) {
@@ -46,6 +47,8 @@ void play_song (std::_List_iterator<Song> &it, std::list<Song> &songs, int num=0
 void action(std::list<Song> songs) {
     char menuselection;
     auto it = songs.begin();
+    std::string new_title, new_artist;
+    int new_rating{0};
 
     do {
         display_menu();
@@ -55,6 +58,7 @@ void action(std::list<Song> songs) {
 
         switch(::toupper(menuselection)) {
             case 'F':
+                it = songs.begin();
                 play_song(it, songs);
                 break;
             case 'N':
@@ -63,9 +67,20 @@ void action(std::list<Song> songs) {
             case 'P':
                 play_song(it, songs, -1);
                 break;
-            // case 'A':
-            //     add_song(songs);
-            //     break;
+            case 'L':
+                display_playlist(songs);
+                break;
+            case 'A':
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                std::cout << "Enter Title: ";
+                std::getline(std::cin, new_title);
+                std::cout << "\nEnter Artist: ";
+                std::getline(std::cin, new_artist);
+                std::cout << "\n And its rating: ";
+                std::cin >> new_rating;
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                songs.emplace(it, Song(new_title, new_artist, new_rating));
+                break;
         }
     } while (::toupper(menuselection) != 'Q');
 }
@@ -73,8 +88,8 @@ void action(std::list<Song> songs) {
 
 int main() {
     Song song1;
-    Song song2{"Genesarets Sjo", "Kjell Höglund", 5};
-    Song song2_5{"Genesarets Sjo", "Kjell Höglund", 5};
+    Song song2{"Genesarets Sjo", "Kjell Hoglund", 5};
+    // Song song2_5{"Genesarets Sjo", "Kjell Hoglund", 5};
     Song song3{"Through the fire and flames", "Dragonball", 4};
 
     std::list songs {song1, song2, song3};
